fefdef0f99067b06dd36c7ed3492186d5f4f8b66
Brian92102 <brian92102@gmail.com>
2016-04-14 17:14:03 -0300

    tp
    struts

---
+++ src/utn/frd/action/PersonaAction.java
@@ -0,0 +1,86 @@
+package utn.frd.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import utn.frd.bean.Persona;
+import utn.frd.db.PersistentManager;
+
+import java.util.List;
+
+public class PersonaAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private String age;
+	private String gender;
+	private List<Persona> personas;
+    
+	
+	
+	public String save(){
+		
+		personas = PersistentManager.getInstance();
+		int edad = 0;
+		
+		try{
+			edad = Integer.parseInt(age);
+		}
+		catch(Exception e){
+			addActionError("Ocurrió un error con la edad");
+			return ERROR;
+		}
+		
+
+		Persona p= new Persona(personas.size(), name, edad, gender);
+		personas.add(p);
+		
+		name=" ";
+		age=" ";
+		gender=" ";
+
+		return SUCCESS;
+		
+		}
+	
+	
+	public String execute(){
+		personas = PersistentManager.getInstance();
+		return SUCCESS;
+		}
+	
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+	public void setAge(String age){
+		this.age=age;
+	}
+	
+	public void setGender(String gender){
+		this.gender=gender;
+	}
+	
+	public void setList(List<Persona> personas){
+		this.personas=personas;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getAge(){
+		return age;
+	}
+	
+	public String getGender(){
+		return gender;
+	}
+	
+	public List<Persona> getList(){
+		return personas;
+	}
+}
---
+++ src/utn/frd/db/PersistentManager.java
@@ -0,0 +1,14 @@
+package utn.frd.db;
+
+import java.util.ArrayList;
+import utn.frd.bean.Persona;
+import java.util.List;
+
+public class PersistentManager {
+	
+	private static List<Persona> data = new ArrayList<Persona>();
+
+	public static List<Persona> getInstance(){
+		return data;
+		}
+}
